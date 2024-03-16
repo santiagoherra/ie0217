986b88fd1cacee34e3b171c10009f43bb7da9983
santiagoherra <santiagoherra@gmail.com>
2024-03-16 16:08:13 -0600

    argumentos

---
+++ Previos/Previo2/holaMundo2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char* argv[]){
+    std::cout << "arg: " << argc << std::endl;
+    std::cout << "argv[0]: " << argv[0] << std::endl;
+    std::cout << "argv[1]: " << argv[1] << std::endl;
+    std::cout << "argv[2]: " << argv[2] << std::endl;
+    std::cout << "argv[3]: " << argv[3] << std::endl;
+
+
+    return 0;
+}
