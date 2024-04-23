c219e5020bc6f19de133931a1090ef6bab839387
santiagoherra <147353623+santiagoherra@users.noreply.github.com>
2024-04-23 09:28:57 -0600

    agregando ultimo archivo de algoritmos

---
+++ Previos/Previo7/stl_algoritmo.cpp
@@ -0,0 +1,30 @@
+#include <iostream>     
+#include <algorithm>    
+#include <vector>       
+
+bool myfunction (int i,int j){ return (i<j);}
+
+struct myClass{
+    bool operator() (int i,int j){return (i<j);}
+} myobject;
+
+int main (){
+    int myints[]= {32, 71, 12, 45, 26, 80, 53, 33};
+    std:: vector <int> myvector (myints, myints+8);     // 32 71 12 45 26 80 53 33
+
+    //Usando el comparador de < 
+    std::sort (myvector.begin(), myvector.begin()+4);   // (12 32 45 71)26 80 53 33
+
+    //Usnado funcion de comparador
+    std::sort (myvector.begin()+4, myvector.end(),myfunction); // 12 32 45 71(26 33 53 80)
+
+    //Usnado objeto de comparador
+    std::sort(myvector.begin(),myvector.end(),myobject);       //(12 26 32 33 45 53 71 80)
+
+    //Imprimir los vectores
+    std:: cout << "myvector contains: ";
+    for (std:: vector<int>::iterator it=myvector.begin(); it!=myvector.end();++it)
+        std::cout<< " " << *it;
+    std::cout<<"\n";
+    return 0;
+}
