6b909fb9067787cc767fcc206a68845cf895ab11
santiagoherra <santiagoherra@gmail.com>
2024-06-20 12:11:12 -0600

    ya casi jeje

--- Tareas/Tarea6/src/main.cpp
+++ Tareas/Tarea6/src/main.cpp
@@ -5,17 +5,5 @@ int main(){
 
     manager.menu();
 
-    //Eliminar cursos inventados
-    manager.eliminar(16);
-    manager.eliminar(17);
-
-    //Eliminar cursos existentes para ver el cambio en requisitos
-    manager.eliminar(10);
-    manager.eliminar(11);
-
-    //Eliminar existente para ver cambios en descripciones
-    manager.eliminar(2);
-    manager.eliminar(4);
-
     return 0;
 }
